27a60be5c1abfe0d938c15e039bf4bb67a2a90b4
slipknot2w1-106416 <63592975+slipknot6666@users.noreply.github.com>
2020-12-27 01:51:19 -0300

    Calculadora js
    modificada

--- javascript - calculadora/js/miCodigo.js
+++ javascript - calculadora/js/miCodigo.js
@@ -1,135 +1,112 @@
-let numero1,
-	numero2,
-	resultado,
-	operacion;
 
-var sumar = function() {
-	document.getElementById("numero").focus();
-	operacion = "suma";
-	validarSiExisteUnNumero();
-	document.getElementById("numero").value = "";
-	mostrarResultadoActual();
+//Gabriel Avendaño Calculadora js actualizada
+
+let box ;
+
+
+function escribir(x){
+ box = document.getElementById("numero");
+
+
+    box.value += x;
+    if(x=="c"||x=="C"){
+      box.value="";
+    }
+  
 }
 
-var mostrarResultado = function() {
-	validarSiExisteUnNumero();
-	if(operacion == "suma"){
-		resultado = numero1 + numero2;
-	}
-	document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
-	numero1 = undefined;
-	numero2 = undefined;
-	document.getElementById("numero").value = "";
+function mostrarResultado(){
+  x=box.value;
+  x=eval(x);
+  box.value=x;
 }
 
-var mostrarResultadoActual = function() {
+function borrarUltimoNumero(){
+  var x = box.value;
+  var despuesDeBorrar= x.length-1;
+  var nuevoNumero = x.substring(0,despuesDeBorrar);
+  box.value = nuevoNumero;
+  
 }
 
-var validarSiExisteUnNumero = function() {
-	if(numero1 == undefined) {
-		numero1 = parseInt(document.getElementById("numero").value);
-		resultado = numero1;
-	} else {
-		numero2 = parseInt(document.getElementById("numero").value);
-	}
+
+function calculo(id){
+  
+  switch(id){
+      
+      case "eleva2":
+        var y=2;
+        var x = box.value;
+        var result =Math.pow(x,y);
+        box.value= result;
+     break;
+      
+      case "eleva3":
+        var y=3;
+        var x = box.value;
+        var result = Math.pow(x,y);
+        box.value= result;
+     break;
+      
+   	  case "logx":
+        var x = box.value;
+        box.value = Math.log(x);
+    break;
+      
+    case "raiz":
+      x=box.value;
+      box.value=Math.sqrt(x);
+      break;
+      
+    case "exponencial":
+      x=box.value;
+      box.value=Math.exp(x);
+      break;
+      
+    case "sen":
+      x=box.value;
+      box.value=Math.sin(x);
+      break;
+      
+    case "asen":
+      x=box.value;
+      box.value=Math.asin(x);
+      break;
+      
+    case "cos":
+      x=box.value;
+      box.value=Math.cos(x);
+      break;
+      
+    case "acos":
+      x=box.value;
+      box.value=Math.acos(x);
+      break;
+      
+    case "tan":
+      x=box.value;
+      box.value = Math.tan(x);
+      break;
+
+    case "atan":
+      x= box.value;
+      box.value= Math.atan(x);
+      break;
+
+    case "factorial":
+      x=box.value;
+      
+      y=1;
+      for(var i=1; i<=x; i++){
+        y=y*i;
+      }
+      box.value=y;
+      
+      break;
+      
+  }
+  
 }
-// function sumare (num1,num2){
-// 	return parseInt(num1)+parseInt(num2);
-// 		}
-// function restare (num1,num2){
-// 	return parseInt(num1)-parseInt(num2);
-// 		}
-//  function dividire (num1,num2){
-// 	return parseInt(num1)/parseInt(num2);
-// 		}
-//  function multiplicare(num1,num2){
-// 	return parseInt(num2)*parseInt(num1);
-// 		}
-//  function potenciare(num1,num2){
-// 	return parseInt(num1**num2);
-// 		}
-//  function calcularResto(num1,num2){
-// 	return parseInt(num1)%parseInt(num2);
-// 		}
-// function raizCuadrada(num){
-// return	Math.sqrt(num);
-// 		}
-// function raizCubica(num){
-// 	return Math.cbrt(num);
-// }
 
 
-// let operacion=0;
 
-// alert("Que operacion desea realizar? 1 suma, 2 resta, 3 dividir, 4 multiplicar,5 potencia, 6 calcular resto")
-// operacion=prompt("Ingrese numero op" );
-
-
-// if(operacion==1){
-
-// 	let numero1=prompt("ingrese n1: ");
-
-// 	let numero2=prompt("ingrese n2: ");
-
-// 	resultado=sumare(numero1,numero2);
-
-// 	alert(`Tu resultado es : ${resultado}`);
-
-// }else if(operacion==2){
-
-// 	let numero1=prompt("ingrese n1: ");
-
-// 	let numero2=prompt("ingrese n2: ");
-
-// 	resultado=restare(numero1,numero2);
-
-// 	alert(resultado);
-
-// }else if(operacion==3){
-
-// 	let numero1=prompt("ingrese n1: ");
-
-// 	let numero2=prompt("ingrese n2: ");
-
-// 	resultado=dividire(numero1,numero2);
-
-// 	alert(resultado);
-
-// }else if(operacion==4){
-
-// 	let numero1=prompt("ingrese n1: ");
-
-// 	let numero2=prompt("ingrese n2: ");
-
-// 	resultado=multiplicare(numero1,numero2);
-
-// 	alert(resultado);
-
-// }else if(operacion==5){
-
-// 	let numero1=prompt("ingrese n1: ");
-
-// 	let numero2=prompt("ingrese n2: ");
-
-// 	resultado=potenciare(numero1,numero2);
-// 	alert(resultado);
-
-// }else if(operacion==6){
-
-// 	let numero1=prompt("ingrese n1: ");
-
-// 	let numero2=prompt("ingrese n2: ");
-
-// 	resultado=calcularResto(numero1,numero2);
-// 	alert(resultado);
-
-// }else if(operacion==7){
-// 	let num=prompt("Ingrese numero");
-
-// 	resultado=raizCuadrada(num);
-// 	alert(resultado);
-// }else if(operacion==8){
-
-// }else{
-// 	alert("no se a encontrado la operación")
-// }
